ac360ddad8361fccda3b69cfb56e84b52c82114d
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2014-02-17 16:00:41 +0100

    fonction dans un fichier  js

--- sources/js/questions.js
+++ sources/js/questions.js
@@ -7,3 +7,27 @@ function quelleheureestil()
     return mon_ts;
 }
 
+
+function getCookie(name)
+{
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+
+function printValue(sliderID, textbox) {
+    var x = document.getElementById(textbox);
+    var y = document.getElementById(sliderID);
+    x.value = y.value;
+}
+
+
+
+function goquestion() {
+   window.location.href="/questionnaire.html";
+}
